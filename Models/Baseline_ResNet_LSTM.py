fdee6733b3c52891949e0f08ae3ecd060383de89
Ashish Farande <ashish.farande@gmail.com>
2022-03-12 22:16:34 -0800

    Transformers init

--- Models/Baseline_ResNet_LSTM.py
+++ Models/Baseline_ResNet_LSTM.py
@@ -1,10 +1,34 @@
 # This model makes use of set of ingredients to predict the ingredients. I have to come up with a proper name 
 # TODO: Not yet completed
 import torch
+import math
 import torch.nn as nn
 
 from torchvision import models
 from model_utils import LossFeature
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        """
+        Args:
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
 
 
 def set_parameter_requires_grad(model, feature_extracting):
@@ -17,7 +41,7 @@ def set_parameter_requires_grad(model, feature_extracting):
 class Baseline_ResNet_LSTM(nn.Module):
     """ ResNet encoder"""
 
-    def __init__(self, outdim=256, dropout_p=0.2, n_lyr=1, num_layer=2, embedding_size=256, hidden_size=512, ing_vocab_size=0 ):
+    def __init__(self, outdim=256, dropout=0.2, n_lyr=1, num_layer=3, embedding_size=256, hidden_size=512, ing_vocab_size=0 , nhead=4):
         super().__init__()
 
         assert ing_vocab_size!=0
@@ -41,9 +65,12 @@ class Baseline_ResNet_LSTM(nn.Module):
         self.graph_to_embedding = nn.Linear(old_output_size, embedding_size)
         self.word_embeddings = nn.Embedding(ing_vocab_size, embedding_size)
 
-        self.ingredient_decoder = nn.LSTM(embedding_size, hidden_size, batch_first=True, num_layers=num_layer)
-        
-        self.hidden_to_word = nn.Linear(hidden_size, ing_vocab_size)
+        # self.ingredient_decoder = nn.LSTM(embedding_size, hidden_size, batch_first=True, num_layers=num_layer)
+        self.pos_encoder = PositionalEncoding(embedding_size, dropout)
+        encoder_layers = TransformerEncoderLayer(embedding_size, nhead)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, num_layer)
+
+        self.hidden_to_word = nn.Linear(embedding_size, ing_vocab_size)
 
         self.sigmoid = nn.Sigmoid()
 
@@ -60,11 +87,12 @@ class Baseline_ResNet_LSTM(nn.Module):
 
         # concat embeds
         inputs = torch.cat((init_embed, ingredients), axis=1)
-        # packed_input = pack_padded_sequence(inputs, sentence.shape[1], batch_first=True)
+        # lstm_out, hidden = self.ingredient_decoder(inputs)
+        
+        inputs = self.pos_encoder(inputs)
+        output = self.transformer_encoder(inputs)
 
-        lstm_out, hidden = self.ingredient_decoder(inputs)
-
-        lstm_feats = self.hidden_to_word(lstm_out)  # perform learn Wh+b
+        lstm_feats = self.hidden_to_word(output)  # perform learn Wh+b
         decoder_out = nn.functional.softmax(lstm_feats[:, :-1, :], dim=2)  # the last prediction makes no sense
         ing_prob = torch.amax(decoder_out, dim=1)
         return ing_prob
